e52f21e33e923f53ddfa1b8e06e49b0eeecb0767
Abhinav Chebrolu <78219591+AbhinavChebrolu@users.noreply.github.com>
2023-04-09 20:51:58 +0530

    Update Handling Errors.py

--- Handling Errors.py
+++ Handling Errors.py
@@ -17,3 +17,28 @@ for i in range(max_retries):
         else:
             logging.error("Max retries exceeded. Aborting.")
             raise
+
+            
+# Insert data into tables
+max_retries = 3
+for owner_id, owner in owners.items():
+    for repo in repos:
+        if repo['owner_id'] == owner_id:
+            for i in range(max_retries):
+                try:
+                    cur.execute('''
+                        INSERT INTO repo (owner_id, name, status, stars_count)
+                        VALUES (%s, %s, %s, %s)
+                        ON CONFLICT DO NOTHING;
+                    ''', (owner_id, repo['name'], repo['status'], repo['stars_count']))
+                    conn.commit()
+                    break
+                except psycopg2.Error as e:
+                    # Log the error and retry the operation
+                    logging.error(f"Error inserting data into database: {e}")
+                    if i < max_retries - 1:
+                        logging.warning(f"Retrying operation ({i+1}/{max_retries})")
+                        time.sleep(5)
+                    else:
+                        logging.error("Max retries exceeded. Aborting.")
+                        raise
