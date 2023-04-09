5397eaba001ed4a5fa31beb70527d31eaab4a8f2
Abhinav Chebrolu <78219591+AbhinavChebrolu@users.noreply.github.com>
2023-04-09 20:37:35 +0530

    Update For Establishing Connection.py

--- For Establishing Connection.py
+++ For Establishing Connection.py
@@ -1,79 +1,84 @@
 import requests
 import psycopg2
+import os
 
-# Github API access token
+# Github OAuth access token
 access_token = "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"
 
-# Database connection details
-db_name = "AbhinavChebrolu"
-db_user = "Abhinavchebrolu"
-db_password = "54321"
-db_host = "localhost"
-db_port = "5432"
+# Postgres database credentials
+dbname = 'AbhinavChebrolu'
+user = 'AbhinavChebrolu'
+password = '54321'
+host = 'localhost'
+port = '5432'
 
-# Github API endpoint
-api_endpoint = "https://api.github.com/user/repos"
-
-# Make a GET request to the Github API
-headers = {"Authorization": f"Bearer {access_token}"}
-response = requests.get(api_endpoint, headers=headers)
-
-# Parse the response JSON and extract required fields
-data = response.json()
-repos = []
-for repo in data:
-    owner_id = repo["owner"]["id"]
-    owner_name = repo["owner"]["login"]
-    owner_email = repo["owner"].get("email")
-    repo_id = repo["id"]
-    repo_name = repo["name"]
-    status = "public" if repo["private"] == False else "private"
-    stars_count = repo["stargazers_count"]
-    repos.append((owner_id, owner_name, owner_email, repo_id, repo_name, status, stars_count))
-
-# Connect to the database and insert data
-conn = psycopg2.connect(
-    dbname=db_name,
-    user=db_user,
-    password=db_password,
-    host=db_host,
-    port=db_port
-)
+# Connect to Postgres database
+conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
 cur = conn.cursor()
 
-# Create the 'repositories' table if it doesn't already exist
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS repositories (
+# Create tables
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS owner (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT
+    );
+''')
+
+cur.execute('''
+    CREATE TABLE IF NOT EXISTS repo (
         id SERIAL PRIMARY KEY,
         owner_id INTEGER NOT NULL,
-        owner_name VARCHAR(255) NOT NULL,
-        owner_email VARCHAR(255),
-        repo_id INTEGER NOT NULL,
-        repo_name VARCHAR(255) NOT NULL,
-        status VARCHAR(10) NOT NULL,
-        stars_count INTEGER NOT NULL
-    )
-""")
+        name TEXT NOT NULL,
+        status TEXT NOT NULL,
+        stars_count INTEGER NOT NULL,
+        FOREIGN KEY (owner_id) REFERENCES owner (id)
+    );
+''')
 
-# Normalize the data to remove duplicates
-repos_normalized = []
-for repo in repos:
-    if repo not in repos_normalized:
-        repos_normalized.append(repo)
+# Fetch data from Github API
+headers = {'Authorization': f'token {access_token}'}
+response = requests.get('https://api.github.com/user/repos', headers=headers)
+data = response.json()
 
-# Insert the normalized data into the 'repositories' table
-for repo in repos_normalized:
-    cur.execute("""
-        INSERT INTO repositories (owner_id, owner_name, owner_email, repo_id, repo_name, status, stars_count) VALUES (%s, %s, %s, %s, %s, %s, %s)""", repo)
+# Normalize and deduplicate data
+owners = {}
+repos = []
+for item in data:
+    owner = item['owner']
+    owner_id = owner['id']
+    if owner_id not in owners:
+        owners[owner_id] = {
+            'name': owner['login'],
+            'email': owner.get('email', '')
+        }
+    repo = {
+        'owner_id': owner_id,
+        'name': item['name'],
+        'status': item['private'] and 'private' or 'public',
+        'stars_count': item['stargazers_count']
+    }
+    repos.append(repo)
 
+# Insert data into tables
+for owner_id, owner in owners.items():
+    cur.execute('''
+        INSERT INTO owner (name, email) VALUES (%s, %s)
+        ON CONFLICT DO NOTHING
+        RETURNING id;
+    ''', (owner['name'], owner['email']))
+    owner_row = cur.fetchone()
+    if owner_row:
+        owner_id = owner_row[0]
+    for repo in repos:
+        if repo['owner_id'] == owner_id:
+            cur.execute('''
+                INSERT INTO repo (owner_id, name, status, stars_count)
+                VALUES (%s, %s, %s, %s)
+                ON CONFLICT DO NOTHING;
+            ''', (owner_id, repo['name'], repo['status'], repo['stars_count']))
+
+# Commit changes and close connection
 conn.commit()
-
-# Print the data from the 'repositories' table
-cur.execute("SELECT * FROM repositories")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
-# Close the database connection
 cur.close()
 conn.close()
